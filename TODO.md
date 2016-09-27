95514e050f675b7de4c984dac44b84e5949e87f9
Benjamin Kircher <kircher@otris.de>
2016-09-27 16:34:34 +0200

    Add DebugConnection.handleResponse
    Make DebugConnection.handleResponse a public method as in
    
        = (response: Response) => { }
    
    Code was passed in constructor as callback function. The use of a lambda
    here is to capture `this`. Strange strange strange.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 # TODO
 
-- write tests for DebugConnection, esp. the response handling code. Maybe make a
-  DebugConnection.handleResponse = (response: Response) => { /* ... */ }
+- write tests for DebugConnection, esp. the response handling code
 - write test/fix behavior when no target is available, try use setTimeout from
   'net' to get a reasonable response time
 - make this all Apache 2.0 licensed if possible, otherwise MIT, remember vscode-firefox-debug
--- src/connection.ts
+++ src/connection.ts
@@ -9,11 +9,9 @@ import { ContextCoordinator } from './context';
 
 let log = Logger.create('DebugConnection');
 
-/** Represents a connection to a target.
- *
- * Following events are emitted:
- *
- * - 'newContext': The target just informed us about a new context. Parameters: contextId, contextName, stopped
+/**
+ * Represents a connection to a target.
+ * @fires DebugConnection.newContext
  */
 export class DebugConnection extends EventEmitter {
     private transport: DebugProtocolTransport;
@@ -26,29 +24,31 @@ export class DebugConnection extends EventEmitter {
         this.responseHandlers = new Map();
         this.coordinator = new ContextCoordinator(this);
         this.transport = new DebugProtocolTransport(socket);
-        this.transport.on('response', (response: Response) => {
-            log.info(`handle response: ${JSON.stringify(response)}`);
+        this.transport.on('response', this.handleResponse);
+    }
 
-            if (response.content.hasOwnProperty('id')) {
-                const uuid: string = response.content.id;
-                if (this.responseHandlers.has(uuid)) {
-                    log.debug(`found a response handler for response id "${uuid}"`);
+    public handleResponse = (response: Response) => {
+        log.info(`handle response: ${JSON.stringify(response)}`);
 
-                    // Meant to be handled by a particular response handler function that was given when sending the
-                    // request
-                    let handler = this.responseHandlers.get(uuid);
-                    try {
-                        handler(response);
-                    } finally {
-                        this.responseHandlers.delete(uuid);
-                    }
-                    return;
+        if (response.content.hasOwnProperty('id')) {
+            const uuid: string = response.content.id;
+            if (this.responseHandlers.has(uuid)) {
+                log.debug(`found a response handler for response id "${uuid}"`);
+
+                // Meant to be handled by a particular response handler function that was given when sending the
+                // request
+                let handler = this.responseHandlers.get(uuid);
+                try {
+                    handler(response);
+                } finally {
+                    this.responseHandlers.delete(uuid);
                 }
+                return;
             }
+        }
 
-            // No response handler; let the context coordinator decide on how to handle the response
-            this.coordinator.handleResponse(response);
-        });
+        // No response handler; let the context coordinator decide on how to handle the response
+        this.coordinator.handleResponse(response);
     }
 
     public disconnect(): Promise<void> {
