6c47b1edb5ca14cbdbe9ebab066eb6b9176b7b40
Benjamin Kircher <benjamin.kircher@gmail.com>
2016-09-28 07:46:02 +0200

    SourceMap: one more test

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -8,16 +8,29 @@ import { SourceMap } from '../src/sourceMap';
 
 suite("source map tests", () => {
 
-    test("remoteSourceUrl", () => {
-        let sourceMap = new SourceMap();
-        sourceMap.setAllSourceUrls([
-            '/usr/lib/fubar/script1.js',
-            '/usr/lib/fubar/extension/script2.js',
-            '/usr/lib/fubar/extension/script3.js',
-            '/usr/lib/fubar/script4.js',
-        ]);
-        let remoteUrl = sourceMap.remoteSourceUrl('/Users/bob/fubar/src/server/script1.js');
-        assert.equal(remoteUrl, '/usr/lib/fubar/script1.js');
+    suite("remoteSourceUrl", () => {
+
+        let sourceMap: SourceMap;
+
+        setup(() => {
+            sourceMap = new SourceMap();
+            sourceMap.setAllSourceUrls([
+                '/usr/lib/fubar/script1.js',
+                '/usr/lib/fubar/extension/script2.js',
+                '/usr/lib/fubar/extension/script3.js',
+                '/usr/lib/fubar/script4.js',
+            ]);
+        });
+
+        test("different paths on localhost and target", () => {
+            let result = sourceMap.remoteSourceUrl('/Users/bob/fubar/src/server/script1.js');
+            assert.equal(result, '/usr/lib/fubar/script1.js');
+        });
+
+        test("fallback to local url", () => {
+            let result = sourceMap.remoteSourceUrl('/Users/bob/fubar/bielefeld.js');
+            assert.equal(result, '/Users/bob/fubar/bielefeld.js')
+        });
     });
 });
 
