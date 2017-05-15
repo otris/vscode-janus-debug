2912a32c9a84f0eaa90ef0d6d3c711ae2b6d5725
Benjamin Kircher <kircher@otris.de>
2017-05-15 11:15:01 +0200

    Wait after calling runScriptOnServer
    Wait with a timeout after SDS connection is established and
    runScriptOnServer is called before try connecting to the debugger port.
    
    This commit fixes launch request for me.
    
    The issue: Connecting to the debugger port regularly resulted in a
    ECONNREFUSED error because the debugger engine was not ready on the
    server. The script was executed and paused correctly but no TCP
    listening socket had been set up so that the launch request failed.
    
    Code desperately needs refactoring. Maybe retry a few time when
    ECONNREFUSED is retrieved.

--- src/debugSession.ts
+++ src/debugSession.ts
@@ -180,6 +180,15 @@ export class JanusDebugSession extends DebugSession {
                 });
 
             }).then(() => {
+                log.info(`begin wait a little`);
+                let wait = new Promise<void>((resolve, reject) => {
+                    setTimeout(() => {
+                        log.info(`done waiting`);
+                        resolve();
+                    }, 3000);
+                });
+                return wait;
+            }).then(() => {
 
                 // Attach to debugger port and tell the frontend that we are ready to set breakpoints.
                 let debuggerSocket = connect(debuggerPort, host);
