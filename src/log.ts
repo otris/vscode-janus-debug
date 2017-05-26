56e7a659d6668d9e5cae2998c64117408e4e0991
Benjamin Kircher <kircher@otris.de>
2017-05-26 16:14:46 +0200

    log: disable lint check only following line

--- src/log.ts
+++ src/log.ts
@@ -98,7 +98,7 @@ export class Logger {
             try {
                 this.logLevel = toNumericLogLevel(Logger._config.logLevel[this.name]);
             } catch (err) {
-                // tslint:disable
+                // tslint:disable-next-line:no-string-literal
                 this.logLevel = toNumericLogLevel(Logger._config.logLevel['default']);
                 throw err;
             }
