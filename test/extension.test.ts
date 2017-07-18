ba96e6b21be4ec0a0eae5253300c16d18f9cbe9e
Marcel Schlitzer <schlitzer@otris.de>
2017-07-18 17:03:07 +0200

    Remove unneeded imports in extension.test.ts

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -3,11 +3,7 @@
 import * as assert from 'assert';
 import { EventEmitter } from 'events';
 import { SocketLike } from 'node-sds';
-import * as vscode from 'vscode';
-import { DebugClient } from 'vscode-debugadapter-testsupport';
 import { cantorPairing, reverseCantorPairing } from '../src/cantor';
-import { ConnectionLike } from '../src/connection';
-import { ContextCoordinator } from '../src/context';
 import { Command, ErrorCode, parseResponse, Response, variableValueToString } from '../src/protocol';
 import { DebugProtocolTransport } from '../src/transport';
 import { VariablesContainer, VariablesMap } from '../src/variablesMap';
