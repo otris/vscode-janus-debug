9623b2849761f38a2f29acca3a6aa1eaf3bd1964
Marcel Schlitzer <schlitzer@otris.de>
2017-07-18 17:18:54 +0200

    Remove cantor tests form extension.test.ts

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -240,16 +240,3 @@ suite('protocol tests', () => {
         });
     });
 });
-
-suite('cantor algorithm tests', () => {
-
-    test('pairing', () => {
-        assert.equal(cantorPairing(47, 32), 3192);
-    });
-
-    test('reverse pairing', () => {
-        const result = reverseCantorPairing(1432);
-        assert.equal(result.x, 52);
-        assert.equal(result.y, 1);
-    });
-});
