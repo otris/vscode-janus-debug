a498257b2c1ba0f9a207d38e19e93b91f0cc0425
Marcel Schlitzer <schlitzer@otris.de>
2017-07-18 15:02:50 +0200

    Add debugSession tests

---
+++ test/debugSession.test.ts
@@ -0,0 +1,52 @@
+'use strict';
+
+import * as assert from 'assert';
+import { EventEmitter } from 'events';
+import { SocketLike } from 'node-sds';
+import { DebugClient } from 'vscode-debugadapter-testsupport';
+import * as debug from '../src/debugSession';
+import { LocalSource, SourceMap } from '../src/sourceMap';
+
+suite('debug Session', () => {
+
+    suite('debug adapter tests', () => {
+
+        class MockSocket extends EventEmitter implements SocketLike {
+            public write(data: Buffer | string, encoding?: string): boolean { return true; }
+            public end(): void { /* */ }
+            public receive(chunk: string): void {
+                this.emit('data', new Buffer(chunk));
+            }
+        }
+
+        const DEBUG_ADAPTER = './out/src/debugSession.js';
+        let debugClient: DebugClient;
+        let socket: MockSocket;
+
+        setup(() => {
+            socket = new MockSocket();
+            debugClient = new DebugClient('node', DEBUG_ADAPTER, 'janus');
+            return debugClient.start();
+        });
+
+        teardown(() => debugClient.stop());
+
+        suite('basic functionality', () => {
+
+            test('initialize: should respond with supported features', done => {
+                return debugClient.initializeRequest().then((response) => {
+                    const body = response.body || {};
+                    assert.equal(body.supportsConfigurationDoneRequest, true);
+                    assert.equal(body.supportsConditionalBreakpoints, false);
+                    done();
+                }).catch(err => done(err));
+            });
+
+            test('disconnect: should return true', async () => {
+                const response = await debugClient.disconnectRequest();
+                assert.equal(response.command, 'disconnect');
+                assert.equal(response.success, true);
+            });
+        });
+    });
+});
