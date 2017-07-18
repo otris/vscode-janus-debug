7fccf4aea9f7bca1896da761af183da5f4008e0a
Marcel Schlitzer <schlitzer@otris.de>
2017-07-18 17:19:11 +0200

    Add cantor.test.ts

---
+++ test/cantor.test.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import * as assert from 'assert';
+import { cantorPairing, reverseCantorPairing } from '../src/cantor';
+
+suite('cantor algorithm tests', () => {
+
+    test('pairing', () => {
+        assert.equal(cantorPairing(47, 32), 3192);
+    });
+
+    test('reverse pairing', () => {
+        const result = reverseCantorPairing(1432);
+        assert.equal(result.x, 52);
+        assert.equal(result.y, 1);
+    });
+});
