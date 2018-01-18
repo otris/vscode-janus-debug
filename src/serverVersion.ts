60cf9c9a486891062f2e78433387ac65bbcb00b9
Christine Heller <heller@otris.de>
2018-01-18 10:36:50 +0100

    move version mapping to another file that will be generated in the future

--- src/serverVersion.ts
+++ src/serverVersion.ts
@@ -1,64 +1,34 @@
+import {versionMapping} from '../portalscript/versions/versionMapping';
 
-/**
- * --- changelog_en.txt ---
- *
- * This Change log includes all error corrections and new functionalities
- * of the following DOCUMENTS build versions:
- *
- * 2045 5.0c HF1                                   (11/2017)
- * 2041 (intermediate version, not an offical HF)  (09/2017)
- * 2040 5.0c                                       (08/2017)
- * 2034 5.0b HF2                                   (04/2017)
- * 2032 5.0b HF1                                   (04/2017)
- * 2033 (intermediate version, not an offical HF)  (03/2017)
- * 2032 5.0b HF1                                   (03/2017)
- * 2030 5.0b                                       (12/2016)
- * 2023 5.0a HF1                                   (09/2016)
- * 2022 5.0a                                       (08/2016)
- * 2020 5.0a beta (not an HF)                      (06/2016)
- * 2015 (intermediate version, not an offical HF)  (05/2016)
- * 2014 5.0 HF2                                    (04/2016)
- * 2013 5.0 HF1                                    (11/2015)
- */
 export function mapVersion(buildVer: string): string {
-    let buildNo = parseInt(buildVer.replace('#', ''), 10);
+    const buildNo = buildVer.replace('#', '');
 
-    if (isNaN(buildNo)) {
-        return '';
+    const keys = Object.keys(versionMapping);
+    if (keys && keys.indexOf(buildNo) >= 0) {
+        return (versionMapping as any)[buildNo];
     }
 
-    if (buildNo < 3000) {
-        buildNo += 6000;
+    return buildNo;
+}
+
+export function findLatestVersion(buildVer: string): string {
+    const buildNo = buildVer.replace('#', '');
+
+    const keys = Object.keys(versionMapping);
+    if (keys && keys.indexOf(buildNo) >= 0) {
+        return (versionMapping as any)[buildNo];
+    } else if (buildNo > "8013") {
+        let idx = keys.findIndex((k, i) => {
+            if (buildNo < k) {
+                return true;
+            }
+            return false;
+        });
+        if (idx === -1) {
+            idx = keys.length;
+        }
+        return (versionMapping as any)[keys[idx - 1]];
     }
 
-    if (buildNo === 2045) {
-        return '5.0c HF1';
-
-    } else if (buildNo === 8040) {
-        return '5.0c';
-
-    } else if (buildNo === 8034) {
-        return '5.0b HF2';
-
-    } else if (buildNo === 8032) {
-        return '5.0b HF1';
-
-    } else if (buildNo === 8030) {
-        return '5.0b';
-
-    } else if (buildNo === 8023) {
-        return '5.0a HF1';
-
-    } else if (buildNo === 8022) {
-        return '5.0a';
-
-    } else if (buildNo === 8014) {
-        return '5.0 HF2';
-
-    } else if (buildNo === 8013) {
-        return '5.0 HF1';
-
-    }
-
-    return buildNo.toString();
+    return "";
 }
