12b1de5ce89293b5c7fde9c7d0ba86b52f923184
Christine Heller <heller@otris.de>
2018-01-28 20:52:41 +0100

    add some comments and rename some varialbes

--- src/serverVersion.ts
+++ src/serverVersion.ts
@@ -9,6 +9,13 @@ const execSync = require('child_process').execSync;
 const PORTALSCRIPTING = 'portalScripting';
 
 
+/**
+ * Return the corresponding version to the build number
+ * given in buildVer if there is any. If no version exists
+ * to this build number, the build number itself is returned.
+ *
+ * @param buildVer build number of the documents server
+ */
 export function getExactVersion(buildVer: string): string {
     const buildNo = buildVer.replace('#', '');
 
@@ -20,6 +27,13 @@ export function getExactVersion(buildVer: string): string {
     return buildNo;
 }
 
+
+/**
+ * Return the latest version that is lower than the
+ * given build number.
+ *
+ * @param buildVer build number of the documents server
+ */
 export function getVersion(buildVer: string): string {
     let buildNo = buildVer.replace('#', '');
     const numberCheck = parseInt(buildNo, 10);
@@ -48,6 +62,12 @@ export function getVersion(buildVer: string): string {
     return "";
 }
 
+
+/**
+ * Check, if the given version is the current latest version.
+ *
+ * @param version version of documents server (e.g. 5.0b HF1)
+ */
 export function isLatestVersion(version: string): boolean {
     const keys = Object.keys(versionMapping);
     keys.sort();
@@ -57,6 +77,11 @@ export function isLatestVersion(version: string): boolean {
     return false;
 }
 
+
+/**
+ * Return the latest build numbert, that is available in
+ * the extension.
+ */
 export function getLatestBuildNo(): string {
     const keys = Object.keys(versionMapping);
     keys.sort();
@@ -64,11 +89,19 @@ export function getLatestBuildNo(): string {
 }
 
 
+/**
+ * If a build number is given and if this is not the latest build of the documents server,
+ * the function gets the corresponding server version and checks if the typings to this
+ * version are already generated. If not, they will be generated.
+ * The typings for the latest version don't have to be generated, because they are always
+ * delivered with the extension.
+ *
+ * @param extensionPath absolute path to extension, in developer mode this is the workspace root
+ * @param outputPath the path where all generated typings are stored
+ * @param buildNo the build number of the documents server
+ */
 export function ensurePortalScriptingTSD(extensionPath: string, outputPath: string, buildNo?: string): string {
     let output = path.join(outputPath, PORTALSCRIPTING + ".d.ts");
-    const nodeModules = path.join(extensionPath, 'node_modules');
-    const typingsPath = path.join(extensionPath, 'portalscript', 'typings');
-    const jsdocPath = path.join(extensionPath, 'portalscript', 'jsdoc');
 
     let version;
     if (buildNo && buildNo !== "") {
@@ -84,20 +117,21 @@ export function ensurePortalScriptingTSD(extensionPath: string, outputPath: stri
         let versionPostfix = version.replace('.', '');
         versionPostfix = versionPostfix.replace(' ', '');
         versionPostfix = versionPostfix.toLocaleLowerCase();
+
         output = path.join(outputPath, PORTALSCRIPTING + "_" + versionPostfix + ".d.ts");
 
         if (!fs.existsSync(output)) {
-            const jsdoc = path.join(nodeModules, '.bin', "jsdoc");
-            const input = path.join(jsdocPath, "portalScripting.js");
-            const concat = path.join(typingsPath, "fileTypeMapper.d.ts");
-            const config = path.join(jsdocPath, 'jsdoc-config.json');
+            const jsdoc = path.join(extensionPath, 'node_modules', '.bin', "jsdoc");
+            const input = path.join(extensionPath, 'portalscript', 'jsdoc', "portalScripting.js");
+            const concat = path.join(extensionPath, 'portalscript', 'typings', "fileTypeMapper.d.ts");
+            const config = path.join(extensionPath, 'portalscript', 'jsdoc', 'jsdoc-config.json');
             const configObject = {
                 "latestVersion": "DOCUMENTS " + version,
-                "versionComparator": path.join(jsdocPath, "versionComparator.js")
+                "versionComparator": path.join(extensionPath, 'portalscript', 'jsdoc', "versionComparator.js")
             };
             const configContent = JSON.stringify(configObject);
             fs.writeFileSync(config, configContent);
-            const template = path.join(nodeModules, '@otris', 'jsdoc-tsd', "src-out", "core");
+            const template = path.join(extensionPath, 'node_modules', '@otris', 'jsdoc-tsd', "src-out", "core");
             execSync(jsdoc + " " + input + " -c " + config + " -t " + template + " -d " + output);
             try {
                 const comment = "// type definition file for portal scripting on DOCUMENTS " + version + "\n";
