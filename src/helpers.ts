447162cefc482cdd3ee9729628c92e874621db21
Christine Heller <heller@otris.de>
2018-08-21 08:42:09 +0200

    move function to settings file

--- src/helpers.ts
+++ src/helpers.ts
@@ -14,11 +14,6 @@ export const COMPARE_FILE_PREFIX = 'compare_';
 
 export const CATEGORY_FOLDER_POSTFIX = '.cat';
 
-export const FORCE_UPLOAD_YES = 'Yes';
-export const FORCE_UPLOAD_NO = 'No';
-export const FORCE_UPLOAD_ALL = 'All';
-export const FORCE_UPLOAD_NONE = 'None';
-export const NO_CONFLICT = 'No conflict';
 
 export const CACHE_FILE = '.vscode-janus-debug';
 const SCRIPT_NAMES_FILE = '.documents-script-names';
@@ -173,81 +168,6 @@ export function getCategoryFromPath(parampath?: string) {
 
 
 
-/**
- * Two things for script are checked:
- *
- * 1) has the category changed?
- * because the category is created from the folder
- * just make sure, that the category is not changed by mistake
- *
- * 2) has the source code on server changed?
- * this is only important if more than one people work on
- * the same server
- *
- * @param script user is asked, if this script should be uploaded
- * @param all true, if user selected 'all scripts should be uploaded'
- * @param none true, if user selected 'no script should be uploaded'
- * @param singlescript true, if user is asked for only one script
- * @param categories true, if the category setting is set
- */
-export async function askForUpload(script: nodeDoc.scriptT, all: boolean, none: boolean, singlescript: boolean, categories: boolean): Promise<string> {
-    return new Promise<string>(async (resolve, reject) => {
-
-        // if lastSyncHash is not set, then confict has been set to true
-        // so actually the case !conflict && !lastSyncHash is not possible here
-        if (!script.conflict && script.lastSyncHash) {
-            return resolve(NO_CONFLICT);
-        }
-
-        let answers = [FORCE_UPLOAD_YES, FORCE_UPLOAD_NO];
-        let question;
-        let answer;
-
-        if (all) {
-            return resolve(FORCE_UPLOAD_ALL);
-        }
-        if (none) {
-            return resolve(FORCE_UPLOAD_NONE);
-        }
-
-        // first check category
-        if (script.conflict && (script.conflict & nodeDoc.CONFLICT_CATEGORY)) {
-            // only show warning, if category feature is used,
-            // if it's not used, categories will never be changed on server
-            // and the warning should be omitted in this case
-            if (categories) {
-                question = `Category of ${script.name} is different on server, upload anyway?`;
-                answer = await vscode.window.showQuickPick(answers, { placeHolder: question });
-            } else {
-                answer = FORCE_UPLOAD_YES;
-            }
-        }
-
-        // if script should not be force uploaded
-        // we do not have to check the source code
-        if (answer === FORCE_UPLOAD_NO) {
-            return resolve(FORCE_UPLOAD_NO);
-        }
-
-        // now check source code
-        if (script.conflict && (script.conflict & nodeDoc.CONFLICT_SOURCE_CODE)) {
-            if (script.encrypted === 'true') {
-                question = `${script.name} cannot be decrypted, source code might have been changed on server, upload anyway?`;
-            } else if (script.lastSyncHash) {
-                question = `Source code of ${script.name} has been changed on server, upload anyway?`;
-            } else {
-                // lastSyncHash not set, so we have no information if the source code on server has been changed
-                question = `Source code of ${script.name} might have been changed on server, upload anyway?`;
-            }
-            if (!singlescript) {
-                answers = [FORCE_UPLOAD_YES, FORCE_UPLOAD_NO, FORCE_UPLOAD_ALL, FORCE_UPLOAD_NONE];
-            }
-            answer = await vscode.window.showQuickPick(answers, { placeHolder: question });
-        }
-
-        return resolve(answer);
-    });
-}
 
 
 
@@ -351,9 +271,9 @@ export async function ensureHiddenFolder(_path: string): Promise<void> {
 /**
  * Check, if the path exists and return the folder path.
  *
- * Todo: allowCreateFolder should be removed, it is very simple
- * to create a folder in VS Code manually and then execute
- * the command on the created folder
+ * Todo: allowCreateFolder should be removed, the extension
+ * will not create folders from user input, folders are
+ * only created for categories
  */
 async function getDir(scriptPath: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
--- src/settings.ts
+++ src/settings.ts
@@ -4,6 +4,12 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import * as helpers from './helpers';
 
+export const FORCE_UPLOAD_YES = 'Yes';
+export const FORCE_UPLOAD_NO = 'No';
+export const FORCE_UPLOAD_ALL = 'All';
+export const FORCE_UPLOAD_NONE = 'None';
+export const NO_CONFLICT = 'No conflict';
+
 // tslint:disable-next-line:no-var-requires
 const fs = require('fs-extra');
 // tslint:disable-next-line:no-var-requires
@@ -94,6 +100,85 @@ export function foldersToCategories(conf: vscode.WorkspaceConfiguration, serverI
 }
 
 
+
+
+
+/**
+ * Two things for script are checked:
+ *
+ * 1) has the category changed?
+ * because the category is created from the folder
+ * just make sure, that the category is not changed by mistake
+ *
+ * 2) has the source code on server changed?
+ * this is only important if more than one people work on
+ * the same server
+ *
+ * @param script user is asked, if this script should be uploaded
+ * @param all true, if user selected 'all scripts should be uploaded'
+ * @param none true, if user selected 'no script should be uploaded'
+ * @param singlescript true, if user is asked for only one script
+ * @param categories true, if the category setting is set
+ */
+async function askForUpload(script: nodeDoc.scriptT, all: boolean, none: boolean, singlescript: boolean, categories: boolean): Promise<string> {
+    return new Promise<string>(async (resolve, reject) => {
+
+        // if lastSyncHash is not set, then confict has been set to true
+        // so actually the case !conflict && !lastSyncHash is not possible here
+        if (!script.conflict && script.lastSyncHash) {
+            return resolve(NO_CONFLICT);
+        }
+
+        let answers = [FORCE_UPLOAD_YES, FORCE_UPLOAD_NO];
+        let question;
+        let answer;
+
+        if (all) {
+            return resolve(FORCE_UPLOAD_ALL);
+        }
+        if (none) {
+            return resolve(FORCE_UPLOAD_NONE);
+        }
+
+        // first check category
+        if (script.conflict && (script.conflict & nodeDoc.CONFLICT_CATEGORY)) {
+            // only show warning, if category feature is used,
+            // if it's not used, categories will never be changed on server
+            // and the warning should be omitted in this case
+            if (categories) {
+                question = `Category of ${script.name} is different on server, upload anyway?`;
+                answer = await vscode.window.showQuickPick(answers, { placeHolder: question });
+            } else {
+                answer = FORCE_UPLOAD_YES;
+            }
+        }
+
+        // if script should not be force uploaded
+        // we do not have to check the source code
+        if (answer === FORCE_UPLOAD_NO) {
+            return resolve(FORCE_UPLOAD_NO);
+        }
+
+        // now check source code
+        if (script.conflict && (script.conflict & nodeDoc.CONFLICT_SOURCE_CODE)) {
+            if (script.encrypted === 'true') {
+                question = `${script.name} cannot be decrypted, source code might have been changed on server, upload anyway?`;
+            } else if (script.lastSyncHash) {
+                question = `Source code of ${script.name} has been changed on server, upload anyway?`;
+            } else {
+                // lastSyncHash not set, so we have no information if the source code on server has been changed
+                question = `Source code of ${script.name} might have been changed on server, upload anyway?`;
+            }
+            if (!singlescript) {
+                answers = [FORCE_UPLOAD_YES, FORCE_UPLOAD_NO, FORCE_UPLOAD_ALL, FORCE_UPLOAD_NONE];
+            }
+            answer = await vscode.window.showQuickPick(answers, { placeHolder: question });
+        }
+
+        return resolve(answer);
+    });
+}
+
 /**
  * Ask user for all conflicted scripts if they should be force uploaded or if upload should
  * be cancelled
@@ -116,19 +201,19 @@ export async function ensureForceUpload(conf: vscode.WorkspaceConfiguration, scr
         // todo: using async/await here probably makes the whole procedure
         // a bit simpler
         return reduce(scripts, (numScripts: number, script: any): Promise<number> => {
-            return helpers.askForUpload(script, all, none, singlescript, categories).then((value) => {
-                if (helpers.NO_CONFLICT === value) {
+            return askForUpload(script, all, none, singlescript, categories).then((value) => {
+                if (NO_CONFLICT === value) {
                     noConflict.push(script);
-                } else if (helpers.FORCE_UPLOAD_ALL === value) {
+                } else if (FORCE_UPLOAD_ALL === value) {
                     script.forceUpload = true;
                     script.conflict = 0;
                     forceUpload.push(script);
                     all = true;
-                } else if (helpers.FORCE_UPLOAD_YES === value) {
+                } else if (FORCE_UPLOAD_YES === value) {
                     script.forceUpload = true;
                     script.conflict = 0;
                     forceUpload.push(script);
-                } else if (helpers.FORCE_UPLOAD_NO === value) {
+                } else if (FORCE_UPLOAD_NO === value) {
                     // do nothing ...
                 } else {
                     // escape or anything should behave as if the user answered 'None'
